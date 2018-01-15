a927d152b92fb1fa8bfbd13597ca360e2db3a706
sAmanzhol <Amanzhol1234567@gmail.com>
2018-01-15 19:27:46 +0600

    First commit

---
+++ SmartReboot/ReadMe.txt
@@ -0,0 +1,40 @@
+========================================================================
+    CONSOLE APPLICATION : SmartReboot Project Overview
+========================================================================
+
+AppWizard has created this SmartReboot application for you.
+
+This file contains a summary of what you will find in each of the files that
+make up your SmartReboot application.
+
+
+SmartReboot.vcxproj
+    This is the main project file for VC++ projects generated using an Application Wizard.
+    It contains information about the version of Visual C++ that generated the file, and
+    information about the platforms, configurations, and project features selected with the
+    Application Wizard.
+
+SmartReboot.vcxproj.filters
+    This is the filters file for VC++ projects generated using an Application Wizard. 
+    It contains information about the association between the files in your project 
+    and the filters. This association is used in the IDE to show grouping of files with
+    similar extensions under a specific node (for e.g. ".cpp" files are associated with the
+    "Source Files" filter).
+
+SmartReboot.cpp
+    This is the main application source file.
+
+/////////////////////////////////////////////////////////////////////////////
+Other standard files:
+
+StdAfx.h, StdAfx.cpp
+    These files are used to build a precompiled header (PCH) file
+    named SmartReboot.pch and a precompiled types file named StdAfx.obj.
+
+/////////////////////////////////////////////////////////////////////////////
+Other notes:
+
+AppWizard uses "TODO:" comments to indicate parts of the source code you
+should add to or customize.
+
+/////////////////////////////////////////////////////////////////////////////
---
+++ SmartReboot/SmartReboot.cpp
@@ -0,0 +1,125 @@
+#include "stdafx.h"
+#include <windows.h>
+#include <iostream>
+#include <string>
+#include <iostream>
+#include <fstream>
+#include <list>  
+
+#define MAX_KEY_LENGTH 255
+#define MAX_VALUE_NAME 16383
+using namespace std;
+void QueryKey(HKEY hKey) {
+	TCHAR    achKey[MAX_KEY_LENGTH];   // buffer for subkey name
+	DWORD    cbName;                   // size of name string 
+	TCHAR    achClass[MAX_PATH] = TEXT("");  // buffer for class name 
+	DWORD    cchClassName = MAX_PATH;  // size of class string 
+	DWORD    cSubKeys = 0;               // number of subkeys 
+	DWORD    cbMaxSubKey;              // longest subkey size 
+	DWORD    cchMaxClass;              // longest class string 
+	DWORD    cValues;              // number of values for key 
+	DWORD    cchMaxValue;          // longest value name 
+	DWORD    cbMaxValueData;       // longest value data 
+	DWORD    cbSecurityDescriptor; // size of security descriptor 
+	FILETIME ftLastWriteTime;      // last write time 
+
+	DWORD i, retCode;
+
+	TCHAR  achValue[MAX_VALUE_NAME];
+	DWORD cchValue = MAX_VALUE_NAME;
+
+	// Get the class name and the value count. 
+	retCode = RegQueryInfoKey(
+		hKey,                    // key handle 
+		achClass,                // buffer for class name 
+		&cchClassName,           // size of class string 
+		NULL,                    // reserved 
+		&cSubKeys,               // number of subkeys 
+		&cbMaxSubKey,            // longest subkey size 
+		&cchMaxClass,            // longest class string 
+		&cValues,                // number of values for this key 
+		&cchMaxValue,            // longest value name 
+		&cbMaxValueData,         // longest value data 
+		&cbSecurityDescriptor,   // security descriptor 
+		&ftLastWriteTime);       // last write time 
+
+	BYTE* buffer = new BYTE[cbMaxValueData];
+	ZeroMemory(buffer, cbMaxValueData);
+	wifstream black_list_file("example.txt");
+	wstring str;
+	list<wstring> black_list;
+	bool isFiltered = false;
+	while (getline(black_list_file, str)) {
+		black_list.push_back(str);
+	}
+	if (cValues) {
+		for (i = 0, retCode = ERROR_SUCCESS; i < cValues; i++) {
+			cchValue = MAX_VALUE_NAME;
+			achValue[0] = '\0';
+			retCode = RegEnumValue(hKey, i,
+				achValue,
+				&cchValue,
+				NULL,
+				NULL,
+				NULL,
+				NULL);
+
+			if (retCode == ERROR_SUCCESS) {
+				DWORD lpData = cbMaxValueData;
+				buffer[0] = '\0';
+				LONG dwRes = RegQueryValueEx(hKey, achValue, 0, NULL, buffer, &lpData);
+				wstring key(&achValue[0]);
+				wstring value(reinterpret_cast<wchar_t*>(buffer), cbMaxValueData / sizeof(wchar_t));
+
+				for (auto key_word : black_list) {
+					if (key.find(key_word) == string::npos && value.find(key_word) == string::npos) {
+						isFiltered = true;
+					}
+					else {
+						isFiltered = false;
+						break;
+					}
+				}
+				
+				if (isFiltered) {
+					BOOL result;
+					SHELLEXECUTEINFO shellExecuteInfo = { 0 };
+					shellExecuteInfo.cbSize = sizeof(shellExecuteInfo);
+					shellExecuteInfo.fMask = SEE_MASK_NOCLOSEPROCESS;
+					shellExecuteInfo.hwnd = NULL;
+					shellExecuteInfo.lpVerb = NULL;
+					shellExecuteInfo.lpFile = value.c_str();
+					shellExecuteInfo.lpParameters = NULL;
+					shellExecuteInfo.lpDirectory = NULL;
+					shellExecuteInfo.nShow = SW_SHOWNORMAL;
+					shellExecuteInfo.hInstApp = NULL;
+					result = ShellExecuteEx(&shellExecuteInfo);
+				}
+				
+			}
+		}
+	}
+
+	delete[] buffer;
+}
+
+
+int _tmain(int argc, _TCHAR* argv[]){
+	HKEY hKey_1;
+	LONG dwRegOPenKey_1 = RegOpenKeyEx(HKEY_LOCAL_MACHINE, _T("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run\\"), 0, KEY_READ, &hKey_1);
+	HKEY hKey_2;
+	LONG dwRegOPenKey_2 = RegOpenKeyEx(HKEY_CURRENT_USER, _T("Software\\Microsoft\\Windows\\CurrentVersion\\Run\\"), 0, KEY_READ, &hKey_2);
+	if (dwRegOPenKey_1 == ERROR_SUCCESS || dwRegOPenKey_2 == ERROR_SUCCESS){
+		QueryKey(hKey_1);
+		QueryKey(hKey_2);
+	}
+	else if (dwRegOPenKey_1 != ERROR_SUCCESS) {
+		printf("RegOpenKeyEx failed, error code %d\n", dwRegOPenKey_1);
+	}
+	else if (dwRegOPenKey_2 != ERROR_SUCCESS) {
+		printf("RegOpenKeyEx failed, error code %d\n", dwRegOPenKey_2);
+	}
+	RegCloseKey(hKey_1);
+	RegCloseKey(hKey_2);
+	return 0;
+}
